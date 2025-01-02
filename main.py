44602e8eddbcc923a6fb437817a622bc9f92e01e
Sh3xe <sh3xe@laposte.net>
2025-01-02 12:17:04 +0100

    méthode de newton

---
+++ main.py
@@ -0,0 +1,28 @@
+from math import exp, cos, sin
+from newton_method import *
+import numpy as np
+
+def test_f(x):
+	""" Function to be optimize using the SQP algorithm """
+	assert x.shape == (5,1)
+	product = 1
+	for v in x:
+		product *= v.item()
+	return exp(product) - 0.5*((x[0][0]**2+x[1][0]**2+1)**2)
+
+def test_g(x):
+	assert x.shape == (5,1)
+	return np.transpose(np.array([[
+		(np.transpose(x)@x - 10)[0,0],
+		x[1,0]*x[2,0] - 5*x[3,0]*x[4,0],
+		1 + x[0,0]**3 + x[1,0]**3
+	]]))
+
+def test():
+	error = sum([abs(calculate_derivative(np.cos, 0, [value]) + sin(value)) for value in np.arange(0.0, 6.28, 0.3)])
+	print(error)
+
+if __name__ == "__main__":
+	x_0 = np.array([[-1.71, 1.59, 1.82, -0.763, -0.763]]).transpose()
+	l_0 = np.ones((3,1))
+	print(newton_method(test_f, test_g, x_0, l_0))
---
+++ newton_method.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+h = 0.01
+
+def calculate_derivative(f, i: int, x: np.array):
+	"""
+		f must take a vector of the same size as x and return a real number
+		i must be between 0 and size(x)-1
+	"""
+	x_a, x_b = x.copy(), x.copy()
+	x_a[i] -= h
+	x_b[i] += h
+	return (f(x_b) - f(x_a)) / (2*h)
+
+def calculate_order_2_derivative(f, i: int, j: int, x: np.array):
+	"""
+		f must take a vector of the same size as x and return a real number
+		i and j must be between 0 and size(x)-1
+	"""
+	a, b, d, c = x.copy(), x.copy(), x.copy(), x.copy()
+	a[i] += h; a[j] +=h
+	b[i] += h; b[j] -=h
+	c[i] -= h; c[j] +=h
+	d[i] -= h; d[j] -=h
+
+	return (1/(h**2)) * (f(a) - f(b) - f(c) + f(d))
+
+def calculate_gradient(f, x):
+	"""
+		f must take a vector of the same size as x and return a real number
+	"""
+	grad = np.zeros(x.shape)
+	for i in range(x.shape[0]):
+		grad[i, 0] = calculate_derivative(f, i, x)
+	return grad
+
+def calculate_jacobian(f, x):
+	"""
+		f must take a vector of the same size as x and return a vector of any size
+	"""
+	jacob = np.zeros((f(x).shape[0], x.shape[0]))
+	for i in range(jacob.shape[0]):
+		f_i = lambda x: f(x)[i]
+		for j in range(jacob.shape[1]):
+			jacob[i,j] = calculate_derivative(f_i, j, x)
+	return jacob
+
+def calculate_hessian(f, x):
+	hess = np.zeros((x.shape[0], x.shape[0]))
+	for i in range(hess.shape[0]):
+		for j in range(hess.shape[1]):
+			hess[i,j] = calculate_order_2_derivative(f, i, j, x)
+	return hess
+
+def newton_method(f, g, x0, l0, num_iters=100):
+	def newton_method_step(x, lamb):
+		l = lambda y: f(y) + (np.transpose(lamb) @ g(y))[0,0]
+
+		Q_tl = calculate_hessian(l, x)
+		Q_bl = calculate_jacobian(g, x)
+		Q_tr = np.transpose(Q_bl.copy())
+		Q_br = np.zeros((lamb.shape[0], lamb.shape[0]))
+		q = np.block([[Q_tl, Q_tr], [Q_bl, Q_br]])
+
+		right_top = calculate_gradient(f, x) + np.transpose(np.transpose(lamb)@calculate_jacobian(g, x))
+		right_bottom = g(x)
+		right = np.concatenate([right_top, right_bottom], 0)
+
+		res = -np.linalg.inv(q) @ right
+		return (x + res[:x.shape[0], [0]], lamb + res[x.shape[0]:, [0]])
+	
+	x_current, l_current = x0, l0
+	for _ in range(num_iters):
+		x_current, l_current = newton_method_step(x_current, l_current)
+
+	return x_current, l_current
